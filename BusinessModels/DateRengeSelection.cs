65c5a1b5c956157a5e38508e17564cd35ee7a622
Marcin Wojcik <marcincyniu@gmail.com>
2024-10-26 14:04:27 -0700

    Add new shared components.

---
+++ BusinessModels/DateRengeSelection.cs
@@ -0,0 +1,8 @@
+namespace NbpExchangeApp.BusinessModels
+{
+    public class DateRangeSelection
+    {
+        public required DateTime StartDate { get; set; }
+        public required DateTime EndDate { get; set; }
+    }
+}
